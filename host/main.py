6e995f358458c409d89448164ad17d5e796a3da3
OliverOhneY <olschmi99@outlook.de>
2026-07-30 00:51:56 +0200

    spotify client data

--- host/main.py
+++ host/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import yaml
 
 
 def run_command(command: str):
@@ -7,4 +8,9 @@ def run_command(command: str):
 
 
 if __name__ == '__main__':
-    run_command('echo "hello from soundCheck"')
+    with open("env", "r") as f:
+        data = yaml.safe_load(f)
+
+    print(data)
+    
+    print(run_command(f"export SPOTIPY_CLIENT_ID='{data['client_id']}' ; export SPOTIPY_CLIENT_SECRET='{data['client_secret']}' ; export SPOTIPY_REDIRECT_URI='example.com'"))
